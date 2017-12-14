5eb10f43bb0e7ef4ec7c46277c694730b73427ce
feilong <feilongphone@gmail.com>
2017-12-14 12:32:03 +0800

    画窗体

--- MnistUi/main.js
+++ MnistUi/main.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+const jQuery = require('jquery')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -13,7 +14,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1024, height: 768})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
